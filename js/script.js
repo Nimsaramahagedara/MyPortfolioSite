2efc3ee6005511de67f1c6fc5e89aeb1c26f91c7
Dilruk Nimsara Mahagedara <92454064+Nimsaramahagedara@users.noreply.github.com>
2022-09-08 15:11:20 +0530

    Js addded

---
+++ js/script.js
@@ -0,0 +1,30 @@
+function changeTheme(){
+    const icon = document.getElementById("theme");
+    const body = document.getElementsByTagName("body")[0];
+    if (icon.className == "fa-regular fa-sun"){
+        icon.className = "fa-solid fa-moon";
+        body.style.background = "white";
+        document.documentElement.style.setProperty('--black', '#4C4646');
+        document.documentElement.style.setProperty('--blur', 'rgba(255, 255, 255, 0.4)');
+        
+    }else{
+        icon.className = "fa-regular fa-sun";
+        body.style.background = "#4C4646";
+        document.documentElement.style.setProperty('--black', 'white');
+        
+        document.documentElement.style.setProperty('--blur', 'rgba(32,30,30, 0.4)');
+    }
+}
+
+function menu(){
+    const nav =  document.getElementsByClassName("menu")[0];
+    var icon = document.getElementById('icon');
+ 
+    if(nav.style.display == "none"){
+         icon.setAttribute("class","fa-solid fa-bars-staggered menuico");
+         nav.style.display = "block";
+    }else{
+         nav.style.display = "none";
+         icon.setAttribute("class","fa-solid fa-bars menuico");
+    }
+ }
